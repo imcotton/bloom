daa2e08df251b6bcfb9dcbd2d70f009a051d1323
Cotton Hou <himcotton@gmail.com>
2024-05-27 07:44:43 +0800

    not export numberToUint8Array

--- bloom.ts
+++ bloom.ts
@@ -20,7 +20,7 @@ function uint8ArrayToNumber(input: Uint8Array) {
  * @param n, the number to be converted
  * @return a Uint8Array representation of the number
  */
-export function numberToUint8Array(n: number): Uint8Array {
+function numberToUint8Array(n: number): Uint8Array {
 
     const result = Array.from({ length: 8 }).reduce(({ acc, x }) => ({
         acc: acc.concat(x & 0xFF),
