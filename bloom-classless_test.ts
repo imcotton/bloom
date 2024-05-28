95b186e75627ad61ca91cf3f9da5ca7deeeccfa9
Cotton Hou <himcotton@gmail.com>
2024-05-29 07:00:19 +0800

    misc. test for modify, at

--- bloom-classless_test.ts
+++ bloom-classless_test.ts
@@ -1,5 +1,6 @@
-import { bloom_by } from "./bloom-classless.ts";
-import { assert } from "jsr:@std/assert@^0.204.0";
+import { bloom_by, at, modify } from "./bloom-classless.ts";
+import { assert, assertEquals } from "jsr:@std/assert@^0.224.0";
+import { describe, it } from "jsr:@std/testing@^0.224.0/bdd";
 
 
 
@@ -22,6 +23,33 @@ Deno.test("should accept AsyncIterable as inserting source", async function () {
     assert(source.every(lookup), "every lookup");
 
     assert(lookup(rand(32)) === false, "false positive");
+
+});
+
+
+
+
+
+describe("Misc", function () {
+
+    it("at", function () {
+        assert(at(Uint8Array.of(9), 0) === 9);
+        assert(at(Uint8Array.of(1), 5) == null);
+    });
+
+    it("modify", function () {
+
+        assertEquals(
+            modify(Uint8Array.from([ 1, 2, 3 ]), 1, () => 9),
+                   Uint8Array.from([ 1, 9, 3 ]),
+        );
+
+        assertEquals(
+            modify(Uint8Array.from([ 1, 2, 3 ]), 5, () => 9),
+                   Uint8Array.from([ 1, 2, 3 ]),
+        );
+
+    });
 
 });
 
