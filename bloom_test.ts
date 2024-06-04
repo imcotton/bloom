779c99b0be7c560887d529afac182d0ff4a41d3e
Cotton Hou <himcotton@gmail.com>
2024-06-04 08:08:56 +0800

    export sample from testing

--- bloom_test.ts
+++ bloom_test.ts
@@ -82,7 +82,7 @@ Deno.test("should no false negative with full buckets", function () {
 
 });
 
-function sample (length: number, bytes = 32) {
+export function sample (length: number, bytes = 32) {
     return Array.from({ length }, function () {
         return crypto.getRandomValues(new Uint8Array(bytes));
     });
