e2aebb8ac56e6c6c23fbb5754eb6cf1d44e0a4e5
Cotton Hou <himcotton@gmail.com>
2025-02-12 21:48:55 +0800

    std v1

--- hashing/fnv1a-deno.ts
+++ hashing/fnv1a-deno.ts
@@ -1,5 +1,5 @@
-import { encodeHex }          from "jsr:@std/encoding@^1.0.0/hex";
-import { crypto as std_crypto } from "jsr:@std/crypto@^1.0.0/crypto";
+import { encodeHex }          from "jsr:@std/encoding@1/hex";
+import { crypto as std_crypto } from "jsr:@std/crypto@1/crypto";
 
 import { modify } from "../bloom-classless.ts";
 
